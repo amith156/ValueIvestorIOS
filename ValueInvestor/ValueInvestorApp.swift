e7f6fafbdce96055d9056abda236c3fb2701307b
Amith's MacBook <amithkumarnarayan1@gmail.com>
2021-06-22 20:16:38 +0100

    Initial Commit

---
+++ ValueInvestor/ValueInvestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValueInvestorApp.swift
+//  ValueInvestor
+//
+//  Created by Amith Narayan on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ValueInvestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
