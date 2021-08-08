7ced3e8d416c68322482650d55553a2d65e95396
Amith's MacBook <aknkum@essex.ac.uk>
2021-08-08 18:30:53 +0100

    A901048-21 created stock row that has all the required data

---
+++ ValueInvestor/Core/Home/Views/StockRowView.swift
@@ -0,0 +1,88 @@
+//
+//  StockRowView.swift
+//  ValueInvestor
+//
+//  Created by Amith Narayan on 08/08/2021.
+//
+
+import SwiftUI
+
+struct StockRowView: View {
+    
+    let getResult : Result
+    let showHoldingsColumn : Bool
+    
+    var body: some View {
+        HStack {
+            
+            columnLeft
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                columnCenter
+            }
+            
+            columnRight
+            
+        }
+        .font(.subheadline)
+    }
+}
+
+struct StockRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            StockRowView(getResult: dev.result, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            StockRowView(getResult: dev.result, showHoldingsColumn: true)
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
+
+
+
+extension StockRowView {
+    
+    
+    private var columnLeft : some View {
+        Text("\(getResult.symbol.uppercased())")
+            .font(.headline)
+            .padding(.leading, 6)
+            .foregroundColor(Color.theme.accent)
+    }
+    
+    
+    private var columnCenter : some View {
+        VStack(alignment: .trailing) {
+            Text(getResult.currentStockHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((getResult.currentStockHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    
+    
+    private var columnRight : some View {
+        VStack(alignment: .trailing) {
+            
+            Text(getResult.ask.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            
+            Text(getResult.regularMarketChangePercent?.asPercentString() ?? "")
+                .foregroundColor(
+                    (getResult.regularMarketChangePercent ?? 0) >= 0 ?
+                        Color.theme.green : Color.theme.red
+                
+                )
+            
+        }
+        .frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing)
+    }
+    
+}
